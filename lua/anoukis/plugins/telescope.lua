50bf483d1d423b906847a4cc06d5e1e000f56747
fafa-a <fafa@DESKTOP-5229KJE>
2024-07-29 23:18:44 +0200

    feat(dark): add telescope

--- lua/anoukis/plugins/telescope.lua
+++ lua/anoukis/plugins/telescope.lua
@@ -2,11 +2,21 @@ local M = {}
 local variant = _O.variants
 function M.setup()
   local theme = {}
+  local colors = {
+    light = {
+      match = {fg = _P[variant .. 300], bg = _P[variant .. 700]},
+      selection = _P[variant .. 300],
+    },
+    dark = {
+      match = {fg = _P[variant .. 700], bg = _P[variant .. 300]},
+      selection = _P[variant .. 700],
+    },
+  }
 
   theme.highlights = {
     TelescopeBorder = { link = "FloatBorder" },
-    TelescopeMatching = { fg = variant and _P[variant .. 600] or _P.purple600, style = _S.bold },
-    TelescopeSelection = { fg = _C.constant, bg = variant and _P[variant .. 300] or _P.purple300 },
+    TelescopeMatching = { fg = colors[_COLOR_BACKGROUND].match.fg, bg = colors[_COLOR_BACKGROUND].match.bg, style = _S.bold },
+    TelescopeSelection = { fg = _C.constant, bg = colors[_COLOR_BACKGROUND].selection },
     TelescopeSelectionCaret = { link = "TelescopeSelection", style = "bold" },
     TelescopeResultsTitle = { fg = _C.constant, style = "bold" },
     TelescopePreviewTitle = { link = "TelescopeResultsTitle" },
