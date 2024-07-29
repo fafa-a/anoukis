8b3c8919e0e77f0ccdecb61b740036cbb201ba6a
fafa-a <fafa@DESKTOP-5229KJE>
2024-07-29 22:28:27 +0200

    feat(dark): add lazy

--- lua/anoukis/plugins/lazy.lua
+++ lua/anoukis/plugins/lazy.lua
@@ -3,11 +3,24 @@ local M = {}
 function M.setup()
   local theme = {}
 
+  local colors= {
+    light = {
+      button = _P.gray200,
+      comment = _P.gray500,
+      commit = _P.slate500,
+    },
+    dark = {
+      button = _P.gray800,
+      comment = _P.gray500,
+      commit = _P.slate500,
+    },
+  }
+
   theme.highlights = {
     LazyNormal = { link = "Normal" },
-    LazyButton = { bg = _P.gray200 },
-    LazyComment = { fg = _P.gray500 },
-    LazyCommit = { fg = _P.slate500 },
+    LazyButton = { bg = colors[_COLOR_BACKGROUND].button },
+    LazyComment = { fg = colors[_COLOR_BACKGROUND].comment },
+    LazyCommit = { fg = colors[_COLOR_BACKGROUND].commit },
     LazyCommitIssue = { link = "WarningMsg" },
     LazyCommitScope = { fg = _C.constant },
     LazyCommitType = { link = "LazyCommitScope" },
