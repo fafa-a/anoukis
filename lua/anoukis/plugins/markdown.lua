322b448a944208e8595a42d3d34ca1695857a982
fafa-a <fafa@DESKTOP-5229KJE>
2024-07-29 22:39:25 +0200

    feat(dark): add markdown

--- lua/anoukis/plugins/markdown.lua
+++ lua/anoukis/plugins/markdown.lua
@@ -2,20 +2,46 @@ local M = {}
 
 function M.setup()
   local theme = {}
-
+  local variant = _O.variants
   --TODO i dont know if this works
+ local colors = {
+    light = {
+      headingDelimiter = _P[variant .. 900],
+      code = _P[variant .. 800],
+      codeBlock = _P[variant .. 700],
+      linkText = _P[variant .. 600],
+      h1 = _P[variant .. 900],
+      h2 = _P[variant .. 800],
+      h3 = _P[variant .. 700],
+      h4 = _P[variant .. 600],
+      h5 = _P[variant .. 500],
+      h6 = _P[variant .. 400],
+    },
+    dark = {
+      headingDelimiter = _P[variant .. 200],
+      code = _P[variant .. 300],
+      codeBlock = _P[variant .. 400],
+      linkText = _P[variant .. 500],
+      h1 = _P[variant .. 200],
+      h2 = _P[variant .. 300],
+      h3 = _P[variant .. 400],
+      h4 = _P[variant .. 500],
+      h5 = _P[variant .. 600],
+      h6 = _P[variant .. 700],
+    },
+  }
 
   theme.highlights = {
-    markdownHeadingDelimiter = { fg = _P.purple900, style = _S.bold },
-    markdownCode = { fg = _P.purple800 },
-    markdownCodeBlock = { fg = _P.purple700 },
-    markdownLinkText = { fg = _P.purple600, style = _S.italic },
-    markdownH1 = { fg = _P.purple900, style = _S.bold },
-    markdownH2 = { fg = _P.purple800, style = _S.bold },
-    markdownH3 = { fg = _P.purple700, style = _S.bold },
-    markdownH4 = { fg = _P.purple600, style = _S.bold },
-    markdownH5 = { fg = _P.purple500, style = _S.bold },
-    markdownH6 = { fg = _P.purple400, style = _S.bold },
+    markdownHeadingDelimiter = { fg = colors[_COLOR_BACKGROUND].headingDelimiter, style = _S.bold },
+    markdownCode = { fg = colors[_COLOR_BACKGROUND].code },
+    markdownCodeBlock = { fg = colors[_COLOR_BACKGROUND].codeBlock },
+    markdownLinkText = { fg = colors[_COLOR_BACKGROUND].linkText, style = _S.italic },
+    markdownH1 = { fg = colors[_COLOR_BACKGROUND].h1, style = _S.bold },
+    markdownH2 = { fg = colors[_COLOR_BACKGROUND].h2, style = _S.bold },
+    markdownH3 = { fg = colors[_COLOR_BACKGROUND].h3, style = _S.bold },
+    markdownH4 = { fg = colors[_COLOR_BACKGROUND].h4, style = _S.bold },
+    markdownH5 = { fg = colors[_COLOR_BACKGROUND].h5, style = _S.bold },
+    markdownH6 = { fg = colors[_COLOR_BACKGROUND].h6, style = _S.bold },
   }
 
   return theme
