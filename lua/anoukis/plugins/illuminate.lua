1160ac7cdf3159d943983a786c304a75b3c1d068
fafa-a <fafa@DESKTOP-5229KJE>
2024-07-29 22:14:02 +0200

    feat(dark): add illuminate

--- lua/anoukis/plugins/illuminate.lua
+++ lua/anoukis/plugins/illuminate.lua
@@ -5,7 +5,7 @@ function M.setup()
   local theme = {}
 
   theme.highlights = {
-    illuminatedWord = { bg = variant and _P[variant .. 200] or _P.purple200, style = _S.none }, -- used for highlighting the current word under the cursor
+    illuminatedWord = { bg = _P[_COLOR_BACKGROUND == 'light'and variant .. 200 or variant .. 700], style = _S.none }, -- used for highlighting the current word under the cursor
     illuminatedWordText = { link = "illuminatedWord" }, -- used for highlighting the fist declaration of the current word under the cursor
     illuminatedWordRead = { link = "illuminatedWord" }, -- used for highlighting all the references of the current word under the cursor
     illuminatedCurWord = { link = "illuminatedWord" }, -- used for highlighting the current word under the cursor
